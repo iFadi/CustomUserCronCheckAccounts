e76c43a5ce8755796153740096b6eb1b2607d032
ifadi <fadi@asbih.com>
2024-03-28 22:54:12 +0200

    fix: fix update version script last try

--- .github/scripts/update_plugin_version.sh
+++ .github/scripts/update_plugin_version.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 echo "Running update_plugin_version.sh..."
-latest_version=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo "Latest version determined: $latest_version"
+
+# Fetch the latest tag and format it.
+latest_version=$(git describe --tags `git rev-list --tags --max-count=1`)
 formatted_version=${latest_version#v}
+echo "Latest version determined: $latest_version"
 echo "Formatted version for update: $formatted_version"
+
+# Display the current version line for diagnostics.
 echo "Current version line in plugin.php:"
 grep "\$version" plugin.php
 
-# Attempting to update plugin.php with latest version, adding verbose output
-sed -i "s/\$version = '[^']*';/\$version = '$formatted_version';/" plugin.php
-echo "Attempted to update plugin.php to version $formatted_version."
+# Attempt to update plugin.php with the new version.
+echo "Attempting to update plugin.php with latest version..."
+sed -i "s/\$version = \"[^\"]*\";/\$version = \"$formatted_version\";/" plugin.php
 
-# Outputting the version line again to verify changes
 echo "Updated version line in plugin.php:"
 grep "\$version" plugin.php
 
-# Check if plugin.php was updated
+# Verify if changes were made and commit if so.
 if ! git diff --exit-code --quiet plugin.php; then
     echo "plugin.php was updated. Proceeding with commit..."
-    git config --global user.email "fadi@asbih.com"
-    git config --global user.name "Fadi Asbih"
+
+    git config user.email "asbih@elsa.uni-hannover.de"
+    git config user.name "Fadi Asbih"
+
     git add plugin.php
     git commit -m "chore: update version in plugin.php to $formatted_version"
 else
